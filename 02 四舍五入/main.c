5d67ec8b9253ce71d179b198176b2a367c59c3d2
Lan-lan-littLE527 <1653105648@qq.com>
2023-10-10 15:09:33 +0800

    添加四舍五入功能文件(测试用)

---
+++ 02 四舍五入/main.c
@@ -0,0 +1,19 @@
+#include"stdio.h"
+int main()
+{
+    float f = 2.35f;
+
+    f = f * 10;
+    f = f + 0.5 ;
+    f = (int)f ;
+    f = f / 10 ;
+
+
+    printf("%f\n", (float)f );
+    while (1)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
