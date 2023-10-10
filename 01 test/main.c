520180a40f02ea1e5d3aa363e521f7a34f40b749
Lan-lan-littLE527 <1653105648@qq.com>
2023-10-10 15:03:47 +0800

    第一次输入

---
+++ 01 test/main.c
@@ -0,0 +1,23 @@
+ #include"stdio.h"
+
+ int main()
+ {
+    int a = 1000;
+    short s = 30000;
+    float f = 0;
+    int i = 0;
+
+    f = a;
+    i = s ;
+    printf("a = %d\n", a );
+    printf("s = %d\n", s );
+    printf("f = %f\n", f );
+    printf("i = %d\n", i );
+
+   s = 3.14f;
+   i = 3.14f;
+
+   printf()
+
+    return 0;
+ }
